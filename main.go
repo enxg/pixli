175725b8531cc9755a82f09f94eefd1713a8f5fe
Enes Genç <hello@enesgenc.dev>
2025-09-13 22:47:24 +0300

    feat: build url shortening ui for logged in users

--- main.go
+++ main.go
@@ -2,6 +2,12 @@ package main
 
 import (
 	"encoding/base64"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
 	"github.com/bytedance/sonic"
 	"github.com/gofiber/fiber/v3"
 	"github.com/gofiber/fiber/v3/client"
@@ -11,9 +17,7 @@ import (
 	"github.com/google/go-querystring/query"
 	_ "github.com/joho/godotenv/autoload"
 	"github.com/rs/zerolog/log"
-	"os"
-	"strconv"
-	"time"
+	"github.com/valyala/fasthttp"
 )
 
 type JWTClaims struct {
@@ -36,14 +40,33 @@ type GHUserResponse struct {
 	ID       int64  `json:"id"`
 }
 
+type IndexData struct {
+	Auth             bool
+	GithubClientID   string
+	TurnstileSiteKey string
+	User             User
+	UntilMin         string
+	UntilMaxEnabled  bool
+	UntilMax         string
+	Notices          []string
+}
+
+type Settings struct {
+	MaxDuration time.Duration
+	Admins      map[string]struct{}
+}
+
 type User struct {
 	ID       string
 	Username string
+	Admin    bool
 }
 
 var UserKey struct{}
 
 func main() {
+	settings := Settings{}
+
 	ghClientId := os.Getenv("GITHUB_CLIENT_ID")
 	ghClientSecret := os.Getenv("GITHUB_CLIENT_SECRET")
 
@@ -52,7 +75,22 @@ func main() {
 		log.Fatal().Err(err).Msg("Failed to decode JWT secret")
 	}
 
-	cc := client.New()
+	maxDurationMinutes, err := strconv.Atoi(os.Getenv("PIXLI_MAX_DURATION"))
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to parse PIXLI_MAX_DURATION")
+	}
+
+	settings.MaxDuration = time.Duration(maxDurationMinutes) * time.Minute
+
+	settings.Admins = make(map[string]struct{})
+
+	for _, admin := range strings.Split(os.Getenv("PIXLI_ADMINS"), ",") {
+		settings.Admins[admin] = struct{}{}
+	}
+
+	cc := client.NewWithClient(&fasthttp.Client{
+		ReadBufferSize: 16384,
+	})
 	cc.SetTimeout(10 * time.Second)
 	cc.SetJSONMarshal(sonic.Marshal)
 	cc.SetJSONUnmarshal(sonic.Unmarshal)
@@ -88,9 +126,12 @@ func main() {
 		}
 
 		if claims, ok := token.Claims.(*JWTClaims); ok {
+			_, isAdmin := settings.Admins[claims.Subject]
+
 			c.Locals(UserKey, User{
 				ID:       claims.Subject,
 				Username: claims.Username,
+				Admin:    isAdmin,
 			})
 		} else {
 			log.Error().Msg("Invalid JWT claims type")
@@ -103,27 +144,22 @@ func main() {
 		logout := fiber.Query[bool](c, "logout")
 		if logout {
 			c.ClearCookie("TOKEN")
-			return c.Render("index", fiber.Map{
-				"Auth":           false,
-				"GithubClientID": ghClientId,
-				"Username":       "",
+			return c.Render("index", IndexData{
+				Auth:           false,
+				GithubClientID: ghClientId,
 			})
 		}
 
 		if user, ok := c.Locals(UserKey).(User); ok {
-			return c.Render("index", fiber.Map{
-				"Auth":           true,
-				"GithubClientID": ghClientId,
-				"Username":       user.Username,
-			})
+			return renderAuth(c, user, settings)
 		}
 
 		authCode := c.Query("code")
 
 		if authCode == "" {
-			return c.Render("index", fiber.Map{
-				"Auth":           false,
-				"GithubClientID": ghClientId,
+			return c.Render("index", IndexData{
+				Auth:           false,
+				GithubClientID: ghClientId,
 			})
 		}
 
@@ -176,10 +212,11 @@ func main() {
 			return c.Status(fiber.StatusInternalServerError).SendString("Internal Server Error")
 		}
 
+		userId := strconv.FormatInt(userResp.ID, 10)
 		claims := JWTClaims{
 			Username: userResp.Username,
 			RegisteredClaims: jwt.RegisteredClaims{
-				Subject:   strconv.FormatInt(userResp.ID, 10),
+				Subject:   userId,
 				IssuedAt:  jwt.NewNumericDate(time.Now()),
 				ExpiresAt: jwt.NewNumericDate(time.Now().Add(8 * time.Hour)),
 			},
@@ -199,12 +236,86 @@ func main() {
 			HTTPOnly: true,
 		})
 
-		return c.Render("index", fiber.Map{
-			"Auth":           true,
-			"GithubClientID": ghClientId,
-			"Username":       userResp.Username,
-		})
+		_, isAdmin := settings.Admins[userId]
+
+		user := User{
+			ID:       userId,
+			Username: userResp.Username,
+			Admin:    isAdmin,
+		}
+
+		return renderAuth(c, user, settings)
+	})
+
+	app.Post("/shorten", func(ctx fiber.Ctx) error {
+
+		return nil
 	})
 
 	_ = app.Listen(":3000")
+}
+
+func renderAuth(c fiber.Ctx, user User, settings Settings) error {
+	now := time.Now()
+	untilMaxEnabled := settings.MaxDuration != time.Duration(0)
+
+	var notices []string
+
+	if user.Admin {
+		notices = append(notices, "You are an admin of this Pixli instance. You are exempt from the limitations.")
+	}
+
+	if untilMaxEnabled {
+		// TODO: I can make this better
+
+		var durationString string
+		totalMinutes := int(settings.MaxDuration.Minutes())
+
+		years := totalMinutes / (365 * 24 * 60)
+		days := (totalMinutes % (365 * 24 * 60)) / (24 * 60)
+		hours := (totalMinutes % (24 * 60)) / 60
+		minutes := totalMinutes % 60
+
+		if years > 0 {
+			durationString += fmt.Sprintf("%d year", years)
+			if years > 1 {
+				durationString += "s"
+			}
+			durationString += " "
+		}
+
+		if days > 0 {
+			durationString += fmt.Sprintf("%d day", days)
+			if days > 1 {
+				durationString += "s"
+			}
+			durationString += " "
+		}
+
+		if hours > 0 {
+			durationString += fmt.Sprintf("%d hour", hours)
+			if hours > 1 {
+				durationString += "s"
+			}
+			durationString += " "
+		}
+
+		if minutes > 0 {
+			durationString += fmt.Sprintf("%d minute", minutes)
+			if minutes > 1 {
+				durationString += "s"
+			}
+		}
+
+		notices = append(notices, fmt.Sprintf("This Pixli instance only allows you to set a maximum duration of %s for shortened URLs.", durationString))
+	}
+
+	return c.Render("index", IndexData{
+		Auth:            true,
+		User:            user,
+		UntilMin:        now.Format(time.RFC3339),
+		UntilMaxEnabled: untilMaxEnabled,
+		UntilMax:        now.Add(settings.MaxDuration).Format(time.RFC3339),
+		Notices:         notices,
+	})
 }
