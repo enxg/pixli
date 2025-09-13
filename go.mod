271beae5efbe39addb7fd5eee2d86796808dc79d
Enes Genç <hello@enesgenc.dev>
2025-09-14 02:08:30 +0300

    feat: add url shortening logic to the backend, along with deletion

--- go.mod
+++ go.mod
@@ -11,7 +11,11 @@ require (
 	github.com/golang-jwt/jwt/v5 v5.3.0
 	github.com/google/go-querystring v1.1.0
 	github.com/joho/godotenv v1.5.1
+	github.com/jxskiss/base62 v1.1.0
+	github.com/robfig/cron/v3 v3.0.1
 	github.com/rs/zerolog v1.34.0
+	github.com/valyala/fasthttp v1.64.0
+	go.mongodb.org/mongo-driver/v2 v2.3.0
 )
 
 require (
@@ -23,6 +27,7 @@ require (
 	github.com/gofiber/template v1.8.3 // indirect
 	github.com/gofiber/utils v1.1.0 // indirect
 	github.com/gofiber/utils/v2 v2.0.0-beta.13 // indirect
+	github.com/golang/snappy v1.0.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/klauspost/compress v1.18.0 // indirect
 	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
@@ -32,11 +37,15 @@ require (
 	github.com/tinylib/msgp v1.3.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasthttp v1.64.0 // indirect
 	github.com/x448/float16 v0.8.4 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.1.2 // indirect
+	github.com/xdg-go/stringprep v1.0.4 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20240726163527-a2c0da244d78 // indirect
 	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
 	golang.org/x/crypto v0.40.0 // indirect
 	golang.org/x/net v0.42.0 // indirect
+	golang.org/x/sync v0.16.0 // indirect
 	golang.org/x/sys v0.34.0 // indirect
 	golang.org/x/text v0.27.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"context"
 	"encoding/base64"
+	"errors"
 	"fmt"
 	"os"
 	"strconv"
@@ -16,12 +18,22 @@ import (
 	"github.com/golang-jwt/jwt/v5"
 	"github.com/google/go-querystring/query"
 	_ "github.com/joho/godotenv/autoload"
+	"github.com/jxskiss/base62"
+	"github.com/robfig/cron/v3"
 	"github.com/rs/zerolog/log"
 	"github.com/valyala/fasthttp"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
 
 type JWTClaims struct {
 	Username string `json:"username"`
+	jwt.RegisteredClaims
+}
+
+type DeletionKeyClaims struct {
+	ShortURL string `json:"short_url"`
 	jwt.RegisteredClaims
 }
 
@@ -49,11 +61,37 @@ type IndexData struct {
 	UntilMaxEnabled  bool
 	UntilMax         string
 	Notices          []string
+	URLs             []URL
+}
+
+type URLListData struct {
+	URLs []URL
 }
 
 type Settings struct {
 	MaxDuration time.Duration
 	Admins      map[string]struct{}
+}
+
+type ShortenRequest struct {
+	URL         string `form:"url"`
+	EnableUntil string `form:"enableUntil"`
+	Until       string `form:"until"`
+	Timezone    string `form:"timezone"`
+}
+
+type Counter struct {
+	CounterID string `bson:"counter_id"`
+	Seq       int64  `bson:"seq"`
+}
+
+type URL struct {
+	CreatedAt   time.Time `bson:"created_at" json:"created_at"`
+	Until       time.Time `bson:"until,omitempty" json:"until,omitempty"`
+	UserID      string    `bson:"user_id" json:"-"`
+	ShortURL    string    `bson:"short_url" json:"short_url"`
+	OriginalURL string    `bson:"original_url" json:"original_url"`
+	DeletionKey string    `bson:"deletion_key" json:"-"`
 }
 
 type User struct {
@@ -65,6 +103,8 @@ type User struct {
 var UserKey struct{}
 
 func main() {
+	ctx := context.Background()
+
 	settings := Settings{}
 
 	ghClientId := os.Getenv("GITHUB_CLIENT_ID")
@@ -87,6 +127,54 @@ func main() {
 	for _, admin := range strings.Split(os.Getenv("PIXLI_ADMINS"), ",") {
 		settings.Admins[admin] = struct{}{}
 	}
+
+	dbc, _ := mongo.Connect(options.Client().ApplyURI(os.Getenv("DATABASE_URL")))
+	defer func() {
+		if err := dbc.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+
+	dbName := os.Getenv("DATABASE_NAME")
+	if dbName == "" {
+		dbName = "pixli"
+	}
+
+	db := dbc.Database(dbName)
+	urlCollection := db.Collection("urls")
+	counterCollection := db.Collection("counters")
+
+	var urlCounter Counter
+	err = counterCollection.FindOne(ctx, bson.D{{"counter_id", "url"}}).Decode(&urlCounter)
+	if errors.Is(err, mongo.ErrNoDocuments) {
+		_, err = counterCollection.InsertOne(ctx, Counter{
+			CounterID: "url",
+			Seq:       0,
+		})
+		if err != nil {
+			log.Fatal().Err(err).Msg("Failed to initialize URL counter")
+		}
+	} else if err != nil {
+		log.Fatal().Err(err).Msg("Failed to get URL counter")
+	}
+
+	c := cron.New()
+	_, err = c.AddFunc("*/5 * * * *", func() {
+		res, err := urlCollection.DeleteMany(ctx, bson.D{{"until", bson.D{{"$lte", time.Now()}}}})
+		if err != nil {
+			log.Error().Err(err).Msg("Failed to clean up expired URLs")
+			return
+		}
+		if res.DeletedCount > 0 {
+			log.Info().Msgf("Cleaned up %d expired URLs", res.DeletedCount)
+		}
+	})
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to schedule URL cleanup job")
+	}
+	c.Start()
+	defer c.Stop()
+	log.Info().Msg("Started URL cleanup cron job")
 
 	cc := client.NewWithClient(&fasthttp.Client{
 		ReadBufferSize: 16384,
@@ -151,7 +239,7 @@ func main() {
 		}
 
 		if user, ok := c.Locals(UserKey).(User); ok {
-			return renderAuth(c, user, settings)
+			return renderAuth(c, ctx, user, settings, urlCollection)
 		}
 
 		authCode := c.Query("code")
@@ -244,18 +332,216 @@ func main() {
 			Admin:    isAdmin,
 		}
 
-		return renderAuth(c, user, settings)
+		return renderAuth(c, ctx, user, settings, urlCollection)
 	})
 
-	app.Post("/shorten", func(ctx fiber.Ctx) error {
+	app.Post("/api/shorten", func(c fiber.Ctx) error {
+		user, ok := c.Locals(UserKey).(User)
+		if !ok {
+			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+				"error": "unauthorized",
+			})
+		}
 
-		return nil
+		req := new(ShortenRequest)
+		if err := c.Bind().Body(req); err != nil {
+			return err
+		}
+
+		var counter Counter
+		err = counterCollection.
+			FindOneAndUpdate(ctx, bson.D{{"counter_id", "url"}}, bson.D{{"$inc", bson.D{{"seq", 1}}}}).
+			Decode(&counter)
+		if err != nil {
+			log.Error().Err(err).Msg("Failed to get and update URL counter")
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": "internal_server_error",
+			})
+		}
+
+		sUrl := string(base62.Encode([]byte(strconv.FormatInt(counter.Seq, 10))))
+		delClaims := DeletionKeyClaims{
+			ShortURL: sUrl,
+			RegisteredClaims: jwt.RegisteredClaims{
+				Subject:  user.ID,
+				IssuedAt: jwt.NewNumericDate(time.Now()),
+			},
+		}
+		delToken := jwt.NewWithClaims(jwt.SigningMethodHS256, delClaims)
+		delKey, err := delToken.SignedString(jwtSecret)
+		if err != nil {
+			log.Error().Err(err).Msg("Failed to sign deletion key JWT token")
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": "internal_server_error",
+			})
+		}
+
+		url := URL{
+			CreatedAt:   time.Now(),
+			UserID:      user.ID,
+			ShortURL:    sUrl,
+			OriginalURL: req.URL,
+			DeletionKey: delKey,
+		}
+
+		if settings.MaxDuration != time.Duration(0) && req.EnableUntil != "on" && !user.Admin {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": "until_required",
+			})
+		}
+
+		if req.EnableUntil == "on" {
+			tzOffsetMin, err := strconv.ParseInt(req.Timezone, 10, 32)
+			if err != nil {
+				return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+					"error": "invalid_until",
+				})
+			}
+			u, err := time.Parse("2006-01-02T15:04", req.Until)
+			if err != nil {
+				return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+					"error": "invalid_until",
+				})
+			}
+
+			u = u.Add(time.Duration(tzOffsetMin) * time.Minute)
+
+			if u.Before(time.Now()) {
+				return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+					"error": "invalid_until",
+				})
+			}
+			if settings.MaxDuration != time.Duration(0) && u.Sub(time.Now()) > settings.MaxDuration && !user.Admin {
+				return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+					"error": "until_too_long",
+				})
+			}
+			url.Until = u
+		}
+
+		_, err = urlCollection.InsertOne(ctx, url)
+		if err != nil {
+			log.Error().Err(err).Msg("Failed to insert URL into database")
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": "internal_server_error",
+			})
+		}
+
+		find, err := urlCollection.Find(ctx, bson.D{{"user_id", user.ID}}, options.Find().
+			SetSort(bson.D{{"created_at", -1}}).
+			SetLimit(10))
+		if err != nil {
+			log.Error().Err(err).Msg("Failed to find URLs for user")
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": "internal_server_error",
+			})
+		}
+		var urls []URL
+		if err = find.All(ctx, &urls); err != nil {
+			log.Error().Err(err).Msg("Failed to decode URLs for user")
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": "internal_server_error",
+			})
+		}
+
+		return c.Render("url-list", URLListData{
+			URLs: urls,
+		})
+	})
+
+	app.Post("/api/delete/:deletionKey", func(c fiber.Ctx) error {
+		delToken, err := jwt.ParseWithClaims(c.Params("deletionKey"), &DeletionKeyClaims{}, func(token *jwt.Token) (interface{}, error) {
+			return jwtSecret, nil
+		})
+		if err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": "invalid_deletion_key",
+			})
+		}
+		if !delToken.Valid {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": "invalid_deletion_key",
+			})
+		}
+
+		claims, ok := delToken.Claims.(*DeletionKeyClaims)
+		if !ok {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": "invalid_deletion_key",
+			})
+		}
+
+		var deletedURL URL
+		err = urlCollection.FindOneAndDelete(ctx, bson.D{{"short_url", claims.ShortURL}, {"user_id", claims.Subject}}).
+			Decode(&deletedURL)
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+				"error": "not_found",
+			})
+		}
+		if err != nil {
+			log.Error().Err(err).Msg("Failed to delete URL from database")
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": "internal_server_error",
+			})
+		}
+
+		find, err := urlCollection.Find(ctx, bson.D{{"user_id", claims.Subject}}, options.Find().
+			SetSort(bson.D{{"created_at", -1}}).
+			SetLimit(10))
+		if err != nil {
+			log.Error().Err(err).Msg("Failed to find URLs for user")
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": "internal_server_error",
+			})
+		}
+		var urls []URL
+		if err = find.All(ctx, &urls); err != nil {
+			log.Error().Err(err).Msg("Failed to decode URLs for user")
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": "internal_server_error",
+			})
+		}
+
+		return c.Render("url-list", URLListData{
+			URLs: urls,
+		})
+	})
+
+	app.Get("/:shortUrl\\+", func(c fiber.Ctx) error {
+		var url URL
+		err := urlCollection.FindOne(ctx, bson.D{{"short_url", c.Params("shortUrl")}}).Decode(&url)
+
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return c.Status(fiber.StatusNotFound).SendString("404 Not Found")
+		}
+		if err != nil {
+			log.Error().Err(err).Msg("Failed to find URL in database")
+			return c.Status(fiber.StatusInternalServerError).SendString("Internal Server Error")
+		}
+
+		return c.JSON(url)
+	})
+
+	app.Get("/:shortUrl", func(c fiber.Ctx) error {
+		var url URL
+		err := urlCollection.FindOne(ctx, bson.D{{"short_url", c.Params("shortUrl")}}).Decode(&url)
+
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return c.Status(fiber.StatusNotFound).SendString("404 Not Found")
+		}
+		if err != nil {
+			log.Error().Err(err).Msg("Failed to find URL in database")
+			return c.Status(fiber.StatusInternalServerError).SendString("Internal Server Error")
+		}
+
+		return c.Redirect().Status(fiber.StatusFound).To(url.OriginalURL)
 	})
 
 	_ = app.Listen(":3000")
 }
 
-func renderAuth(c fiber.Ctx, user User, settings Settings) error {
+func renderAuth(c fiber.Ctx, ctx context.Context, user User, settings Settings, urlCollection *mongo.Collection) error {
 	now := time.Now()
 	untilMaxEnabled := settings.MaxDuration != time.Duration(0)
 
@@ -308,6 +594,24 @@ func renderAuth(c fiber.Ctx, user User, settings Settings) error {
 		}
 
 		notices = append(notices, fmt.Sprintf("This Pixli instance only allows you to set a maximum duration of %s for shortened URLs.", durationString))
+		notices = append(notices, "URLs shortened via ShareX will be set to the maximum duration.")
+	}
+
+	find, err := urlCollection.Find(ctx, bson.D{{"user_id", user.ID}}, options.Find().
+		SetSort(bson.D{{"created_at", -1}}).
+		SetLimit(10))
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to find URLs for user")
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "internal_server_error",
+		})
+	}
+	var urls []URL
+	if err = find.All(ctx, &urls); err != nil {
+		log.Error().Err(err).Msg("Failed to decode URLs for user")
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "internal_server_error",
+		})
 	}
 
 	return c.Render("index", IndexData{
@@ -317,5 +621,6 @@ func renderAuth(c fiber.Ctx, user User, settings Settings) error {
 		UntilMaxEnabled: untilMaxEnabled,
 		UntilMax:        now.Add(settings.MaxDuration).Format(time.RFC3339),
 		Notices:         notices,
+		URLs:            urls,
 	})
 }
